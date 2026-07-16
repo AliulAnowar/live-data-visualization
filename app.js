33eaf311d21942386e2f3e9b66c55006e2877f58
AliulAnowar <55461139+AliulAnowar@users.noreply.github.com>
2026-07-16 21:41:36 +0600

    Update app.js
    hidden supabase key through process key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // --- app.js: Optimized M&E Enterprise Portal Logic ---
 const { createClient } = supabase;
-const supabaseUrl = 'https://eghmzetfcimllmenhhei.supabase.co';
-const supabaseKey = 'sb_publishable_xHtk1KS8h_c7Xp26RoMk0w_EbUifBgQ';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
 let currentUserProfile = null;
 // 2. THIS LISTENER ONLY SETS UP THE UI
